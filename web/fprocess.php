3255121f1d78c107539ffa234c2a7b006d6595f5
ntmtri23 <ntmtri23@gmail.com>
2015-09-15 03:32:05 +0700

    aa

--- web/fprocess.php
+++ web/fprocess.php
@@ -1,19 +1,25 @@
 <?php
-session_start();
-// Set the location of the Facebook PHP SDK - this should point to directory containing autoload.php
-define( 'FACEBOOK_SDK_V4_SRC_DIR', './Facebook/' );
-// include required files from Facebook SDK
-require_once( 'facebook/autoload.php' );
-// setup application using API keys and handlers
-$fb = new Facebook\Facebook([
-  'app_id' => '1616272128625137',
-  'app_secret' => 'e9cffe45e3f14a54dc0d311ed3efb4b2',
-  'http_client_handler' => 'curl', // can be changed to stream or guzzle
-  'persistent_data_handler' => 'session' // make sure session has started
-]);
-
-$helper = $fb->getRedirectLoginHelper();
-$permissions = ['email']; // optional
-$loginUrl = $helper->getLoginUrl('http://fbappapp.herokuapp.com/login-callback.php', $permissions);
-
-echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+try
+{
+    session_start();
+    require_once( 'facebook/autoload.php' );
+    // setup application using API keys and handlers
+    $fb = new Facebook\Facebook([
+      'app_id' => '1616272128625137',
+      'app_secret' => 'e9cffe45e3f14a54dc0d311ed3efb4b2',
+      'http_client_handler' => 'curl', // can be changed to stream or guzzle
+      'persistent_data_handler' => 'session' // make sure session has started
+    ]);
+    
+    $helper = $fb->getRedirectLoginHelper();
+    $permissions = ['email']; // optional
+    $loginUrl = $helper->getLoginUrl('http://fbappapp.herokuapp.com/login-callback.php', $permissions);
+    
+    echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+}
+catch(exception $ex)
+{
+    var_dump($ex);
+}
