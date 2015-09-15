ee0a88b9142dcdb52537b6f6512180a1f18d648a
ntmtri23 <ntmtri23@gmail.com>
2015-09-16 00:18:36 +0700

    Next

--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@
 <script type="text/javascript" src="fancybox/source/jquery.fancybox.js?v=2.1.5"></script>
 <link rel="stylesheet" type="text/css" href="fancybox/source/jquery.fancybox.css?v=2.1.5" media="screen" />
 
+
+
+
 <script type="text/javascript">
 	$(document).ready(function () {
 		$('.fancybox').fancybox();
@@ -14,15 +17,41 @@
     function PreSpin() {
         $(".show-form-infomation").click();
     }
-   
 </script>
+
+<?php
+try
+{
+    session_start();
+    require_once( 'facebook/autoload.php' );
+    // setup application using API keys and handlers
+    $fb = new Facebook\Facebook([
+      'app_id' => '1616272128625137',
+      'app_secret' => 'e9cffe45e3f14a54dc0d311ed3efb4b2',
+      'http_client_handler' => 'curl', // can be changed to stream or guzzle
+      'persistent_data_handler' => 'session' // make sure session has started
+    ]);
+    
+    $helper = $fb->getRedirectLoginHelper();
+    $permissions = ['email','user_birthday']; // optional
+    $loginUrl = $helper->getLoginUrl('https://fbappapp.herokuapp.com/login-callback.php', $permissions);
+    
+    echo '<a href="' . $loginUrl . '" target="_top">Log in with Facebook!</a>';
+}
+catch(exception $ex)
+{
+    var_dump($ex);
+	echo 111;
+}
+?>
+
 <div class="row">
     <div class="the_wheel" align="center" valign="center" style="width:760px;height:582px;padding-top:100px; border-top:1px solid #d9ac1a;-webkit-box-shadow: 2px 1px 22px 7px rgba(0,0,0,0.75);-moz-box-shadow: 2px 1px 22px 7px rgba(0,0,0,0.75);box-shadow: 2px 1px 22px 7px rgba(0,0,0,0.75);float:left;-webkit-border-radius: 10px;-moz-border-radius: 10px;border-radius: 10px;background:url('img/bg006.jpg')">
         <canvas class="the_canvas" id="myDrawingCanvas" width="434" height="434" onclick="PreSpin();" style="cursor:pointer;">
             <p class="noCanvasMsg" align="center">Ôi không?.Trình duy?t c?a b?n không h? tr? html5<br />Hãy nâng c?p nó.</p>
         </canvas>
     </div>
-    <a href="fprocess.php" class="show-form-infomation">#EE</a>
+    <a href="<?php echo $loginUrl; ?>" class="show-form-infomation" style="display: none;">#EE</a>
     <a id="various2" href="#inline2" style="display:none;" class="show-result-spin">#EE</a>
     <input type="hidden" value="@ViewBag.LuckyDrawId" id="lkdit" />
     <input type="hidden" value="img/luckydrawab3.png" id="spinImagePath" />
