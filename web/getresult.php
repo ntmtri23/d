f60e9a36449857808fca47868d0e85bc3f1da78e
ntmtri23 <ntmtri23@gmail.com>
2015-10-04 02:02:13 +0700

    Next

--- web/getresult.php
+++ web/getresult.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
-    $totalItem = 8;
-    $xml = file_get_contents('http://localhost:7749/LuckyDraw/GetItemSpin/?luckyDrawId=401a0f70-cc1d-e511-941c-001c42aaff6e');
+    /*$totalItem = 8;
+    $xml = file_get_contents('http://demo.trathuong.com/LuckyDraw/GetItemSpin/?luckyDrawId=401a0f70-cc1d-e511-941c-001c42aaff6e');
 	$itemR = json_decode($xml);
     
     $itemRand = 360 / $totalItem;
@@ -9,5 +9,7 @@
     $endVal = $itemRand * $itemR->Order;
     $reponseVal = rand($startVal,$endVal);
     $_SESSION['ItemId'] = $itemR->ID;
-    echo $reponseVal;
+    echo $reponseVal();
+    */
+    echo '124';
 ?>
