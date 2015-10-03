98ebe1cc724b8d9898c5b8599812fb5ef73d48f0
ntmtri23 <ntmtri23@gmail.com>
2015-10-04 02:13:30 +0700

    Next

--- web/login-callback.php
+++ web/login-callback.php
@@ -65,7 +65,7 @@
     {
         //var_dump($ex);
     }
-    header('Location:index.php');
+    header('Location:https://apps.facebook.com/luckydraw-app');
     $itemList = '';
     $itemR = simplexml_load_file('http://fbapp.trathuong.com/ActionService.asmx/GetItemList?luckydrawId=401a0f70-cc1d-e511-941c-001c42aaff6e');
 	foreach($itemR->LuckyDrawItemModel as $item)
