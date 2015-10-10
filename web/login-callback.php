1563e361b00e92b4979a3fb3cf32208f91ba04fb
ntmtri23 <ntmtri23@gmail.com>
2015-10-10 23:00:12 +0700

    Next

--- web/login-callback.php
+++ web/login-callback.php
@@ -59,9 +59,9 @@
           $request = $fb->get('/me?fields=id,name,gender,link,picture,age_range', $_SESSION['facebook_access_token']);
           // Exchange the short-lived token for a long-lived token.
           //$response = $request->execute();
-          //$graphObject = $response->getGraphObject();
+          $graphObject = $response->getGraphObject();
           //$_SESSION['CurrentLoginUser'] = $response->getGraphUser();
-          var_dump($request);
+          var_dump($graphObject);
         }
     }
     catch(exception $ex)
